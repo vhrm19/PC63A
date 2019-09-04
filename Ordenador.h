b6770e5740c9bd9e2e4b7081e3420b9cd3141ede
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2019-09-04 15:01:34 -0300

    Update Ordenador.h

--- Ordenador.h
+++ Ordenador.h
@@ -1,8 +1,29 @@
+/**
+ * @file Ordenador.h
+ * @author Victor Hugo Rodrigues Machado
+ * @date 04 Sep 2019
+ * @brief Implementacao dos métodos de ordenacao
+ * Implementa os metodos de ordenacao vistos na disciplina
+ * pesquisa e classificacao de dados.
+ */ 
+
 #ifndef ORDENADOR_H_INCLUDED
 #define ORDENADOR_H_INCLUDED
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * \brief Ordena o vetor usando BubbleSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param N tamanho do vetor
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Bubble Sort
+ * Esse algorimo tem um comportamento assintótico O (N²)
+ */
 
 void BubbleSort(int *vetor, int N, int *comparacao, int *troca){
     int aux;
@@ -22,6 +43,18 @@ void BubbleSort(int *vetor, int N, int *comparacao, int *troca){
     }while(check == 1);
 };
 
+/**
+ * \brief Ordena o vetor usando HeapSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param N tamanho do vetor
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Heap Sort
+ * Esse algorimo tem um comportamento assintótico O (N*log(N))
+ */
+
 void heapSort(int *vetor, int N, int *comparacao, int *troca){
     int aux;
     for(int i = (N-1)/2; i >= 0; i--)
@@ -35,6 +68,18 @@ void heapSort(int *vetor, int N, int *comparacao, int *troca){
         (*troca)++;
     }
 }
+
+/**
+ * \brief Funcao auxiliar para ordenar o vetor como um heap maximo
+ *
+ * \param vetor vetor a ser ordenado
+ * \param raiz indice do elemento pai
+ * \param N tamanho do vetor
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Organiza o vetor como um heap maximo para que possa ser realizado um Heap Sort
+ */
 
 void maxHeap(int *vetor, int raiz, int N, int *comparacao, int *troca){
     int aux = vetor[raiz];
@@ -56,6 +101,18 @@ void maxHeap(int *vetor, int raiz, int N, int *comparacao, int *troca){
     vetor[raiz] = aux;
 }
 
+/**
+ * \brief Ordena o vetor usando InsertionSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param N tamanho do vetor
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Insertion Sort
+ * Esse algorimo tem um comportamento assintótico O (N²)
+ */
+
 void InsertionSort(int *vetor, int N, int *comparacao, int *troca){
     int aux, j;
     for(int i=1; i<N; i++){
@@ -69,6 +126,19 @@ void InsertionSort(int *vetor, int N, int *comparacao, int *troca){
     }
 };
 
+/**
+ * \brief Ordena o vetor usando MergeSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param inicio posicao a qual o vetor utilizado comeca
+ * \param fim posicao a qual o vetor utilizado acaba
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Merge Sort
+ * Esse algorimo tem um comportamento assintótico O (N*log(N))
+ */
+
 void MergeSort(int *vetor, int inicio, int fim, int *comparacao, int *troca){
     int meio = floor((inicio+fim)/2);
     if(inicio < fim){
@@ -77,6 +147,19 @@ void MergeSort(int *vetor, int inicio, int fim, int *comparacao, int *troca){
         merge(vetor, inicio, meio, fim, comparacao, troca);
     }
 }
+
+/**
+ * \brief Funcao auxiliar para a funcao MergeSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param inicio posicao a qual o vetor utilizado comeca
+ * \param fim posicao a qual o vetor utilizado acaba
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Utiliza um vetor auxiliar para poder armazenar os
+ * dados a serem organizados por intercalacao
+ */
 
 void merge(int *vetor, int inicio, int meio, int fim, int *comparacao, int *troca){
     int *vetor_aux = (int *) calloc(fim-inicio+1, sizeof(int));
@@ -120,6 +203,19 @@ void merge(int *vetor, int inicio, int meio, int fim, int *comparacao, int *troc
     vetor_aux = NULL;
 }
 
+/**
+ * \brief Ordena o vetor usando QuickSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param inicio posicao a qual o vetor utilizado comeca
+ * \param fim posicao a qual o vetor utilizado acaba
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Quick Sort
+ * Esse algorimo tem um comportamento assintótico O (N*log(N))
+ */
+
 void quickSort(int *vetor, int inicio, int fim, int *comparacao, int *troca){
     int pivo;
     if(inicio < fim){
@@ -128,6 +224,18 @@ void quickSort(int *vetor, int inicio, int fim, int *comparacao, int *troca){
         quickSort(vetor, pivo+1, fim, comparacao, troca);
     }
 }
+
+/**
+ * \brief Funcao auxiliar para a funcao quickSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param inicio posicao a qual o vetor utilizado comeca
+ * \param fim posicao a qual o vetor utilizado acaba
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Organiza os dados a partir de um pivo pre definido
+ */
 
 int particiona(int *vetor, int inicio, int fim, int *comparacao, int *troca){
     int pivo = vetor[inicio];
@@ -158,6 +266,18 @@ int particiona(int *vetor, int inicio, int fim, int *comparacao, int *troca){
     return dir;
 }
 
+/**
+ * \brief Ordena o vetor usando SelectionSort
+ *
+ * \param vetor vetor a ser ordenado
+ * \param N tamanho do vetor
+ * \param comparacao contador de comparacoes feitas pelo algoritmo
+ * \param troca contador de trocas feitas pelo algoritmo
+ *
+ * Ordena o vetor usando o método Selection Sort
+ * Esse algorimo tem um comportamento assintótico O (N²)
+ */
+
 void SelectionSort(int *vetor, int N, int *comparacao, int *troca){
         int aux;
         int pos = 0;
@@ -176,21 +296,6 @@ void SelectionSort(int *vetor, int N, int *comparacao, int *troca){
             }
             (*comparacao)++;;
         }
-    };
-
-    int *alea(int N){
-        int *vetor = (int *) malloc(N*sizeof(int));
-        srand(time(NULL));
-        for(int i=0; i<N; i++)
-            vetor[i] = rand()%N;
-        return vetor;
-    }
-
-    void exibeVetor(int *vetor, int N){
-        for(int i=0; i<N; i++){
-        printf("%i ", vetor[i]);
-        }
-        printf("\n");
     };
 
 #endif // ORDENADOR_H_INCLUDED
