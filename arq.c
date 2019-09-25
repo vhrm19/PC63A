fc73bf087daf0f01bc5187110109c7aea9b25597
Victor Hugo <42815419+vhrm19@users.noreply.github.com>
2019-09-25 09:11:00 -0300

    Create arq.c

---
+++ arq.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct{
+    char *nome;
+    char *cidade;
+    char *curso;
+}Aluno;
+
+
+int main(){
+    int num_cadastros = 5;
+    char buffer[100];
+    Aluno cadastro[num_cadastros];
+    FILE *saida;
+    saida = fopen("alunos.txt", "w");
+    for(size_t i = 0; i < num_cadastros; i++){   
+        printf("Digite o nome do aluno: ");
+        scanf("%[^\n]s", buffer);
+        cadastro[i].nome = (char *) malloc(strlen(buffer)*sizeof(char));
+        strcpy(cadastro[i].nome, buffer);
+        __fpurge(stdin);
+
+        printf("Digite a cidade do aluno: ");
+        scanf("%[^\n]s", buffer);
+        cadastro[i].cidade = (char *) malloc(strlen(buffer)*sizeof(char));
+        strcpy(cadastro[i].cidade, buffer);
+        __fpurge(stdin);
+
+        printf("Digite a curso do aluno: ");
+        scanf("%[^\n]s", buffer);
+        cadastro[i].curso = (char *) malloc(strlen(buffer)*sizeof(char));
+        strcpy(cadastro[i].curso, buffer);
+        __fpurge(stdin);
+
+        fprintf(saida, "%-50.50s", cadastro[i].nome);
+        fprintf(saida, "%-50.50s", cadastro[i].cidade);
+        fprintf(saida, "%-50.50s", cadastro[i].curso);
+    }
+    fflush(saida);
+    fclose(saida);
+    return 0;
+}
